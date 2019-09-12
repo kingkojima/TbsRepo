237efb03865884fb80fb82eb6b4a1942c43397b9
TBS-System-Administr <TBS-System-Administr@TBS-System-Admi>
2019-09-12 19:34:46 +0900

    DTO 1つ完成

---
+++ src/jp/ne/tbs/frame/DB/DB00A001Z00.java
@@ -0,0 +1,76 @@
+package jp.ne.tbs.frame.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>[クラス名]</p>
+ * 　　DATA_PATIENTテーブル　DAOクラス
+ * <p>[概要]</p>
+ * 　　Ciao用のSQL Server 2016 の DATA_PATIENTテーブルより</br>
+ * 　　データを取得し、DTOに格納するクラス。
+ * <p>[変更履歴]</p>
+ * 　　2019/09/12　小嶋純史　新規作成
+ */
+public class DB00A001Z00 {
+
+	/** データベースのURL */
+	private static final String dbURL = "jdbc:sqlserver://192.168.11.10";
+	/** ユーザーID */
+	private static final String user = "sa";
+	/** パスワード */
+	private static final String pass = "supply";
+	/** 発行SQL */
+	private static final String SQL = "select * from Ciao.dbo.DATA_PATIENT order by ID";
+
+	/**
+	 * <p>[メソッド名] </p>
+	 * @param
+	 * @return
+	 */
+	public List<DB00T001Z00> findAll() {
+
+		List<DB00T001Z00> dtoList = new ArrayList<>();
+
+		// コネクション定義
+		Connection conn = null;
+		PreparedStatement ps = null;
+
+		// 接続開始
+		try {
+			//コネクション取得
+			conn = DriverManager.getConnection(dbURL, user, pass);
+			//SQL設定
+			ps = conn.prepareStatement(SQL);
+			//SQLを発行し、結果取得
+			try (ResultSet rs = ps.executeQuery()) {
+				//結果をDTOに格納
+				while (rs.next()) {
+					DB00T001Z00 dto = new DB00T001Z00();
+					dto.setId(rs.getString("id"));
+					dto.setFname(rs.getString("fname"));
+					dto.setAname(rs.getString("aname"));
+					dtoList.add(dto);
+				}
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return dtoList;
+	}
+}
---
+++ src/jp/ne/tbs/frame/DB/DB00T001Z00.java
@@ -0,0 +1,117 @@
+package jp.ne.tbs.frame.DB;
+
+/**
+ * <p>[概要]</p>
+ * 　　DATA_PATIENTテーブル　DTOクラス
+ * <p>[詳細]</p>
+ * <p>[備考]</p>
+ * <p>[メモ]</p>
+ * @since 2019/9/12
+ * @version 1.0.0
+ * @author TBS-System-Administr
+ */
+public class DB00T001Z00 {
+
+	/**
+	 * 患者ID
+	 */
+	private String id;
+
+	/**
+	 * 患者の漢字名
+	 */
+	private String fname;
+
+	/**
+	 * 患者のカナ名
+	 */
+	private String aname;
+
+	/**
+	 * 患者の性別
+	 */
+	private int sex;
+
+	/**
+	 * <p>[概 要] id を取得する。</p>
+	 * <p>[詳 細] </p>
+	 * <p>[備 考] </p>
+	 * @return id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * <p>[概 要] id を設定する。</p>
+	 * <p>[詳 細] </p>
+	 * <p>[備 考] </p>
+	 * @param id セットする
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * <p>[概 要] fname を取得する。</p>
+	 * <p>[詳 細] </p>
+	 * <p>[備 考] </p>
+	 * @return fname
+	 */
+	public String getFname() {
+		return fname;
+	}
+
+	/**
+	 * <p>[概 要] fname を設定する。</p>
+	 * <p>[詳 細] </p>
+	 * <p>[備 考] </p>
+	 * @param fname セットする
+	 */
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	/**
+	 * <p>[概 要] aname を取得する。</p>
+	 * <p>[詳 細] </p>
+	 * <p>[備 考] </p>
+	 * @return aname
+	 */
+	public String getAname() {
+		return aname;
+	}
+
+	/**
+	 * <p>[概 要] aname を設定する。</p>
+	 * <p>[詳 細] </p>
+	 * <p>[備 考] </p>
+	 * @param aname セットする
+	 */
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+
+	/**
+	 * <p>[概 要] sex を取得する。</p>
+	 * <p>[詳 細] </p>
+	 * <p>[備 考] </p>
+	 * @return sex
+	 */
+	public int getSex() {
+		return sex;
+	}
+
+	/**
+	 * <p>[概 要] sex を設定する。</p>
+	 * <p>[詳 細] </p>
+	 * <p>[備 考] </p>
+	 * @param sex セットする
+	 */
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+
+
+
+}
