81f90a2264c560d3344676e03963c20bf5e33edd
TBS-System-Administr <TBS-System-Administr@TBS-System-Admi>
2020-07-30 18:30:10 +0900

    2020/7/30 修正

--- src/jp/ne/tbs/view/RE01/MRE01V001Z00.java
+++ src/jp/ne/tbs/view/RE01/MRE01V001Z00.java
@@ -30,8 +30,6 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-import com.github.lgooddatepicker.components.DatePicker;
-
 import jp.ne.tbs.frame.AA00.JTextFieldEx;
 import jp.ne.tbs.frame.AA00.MAAT00;
 
@@ -50,6 +48,7 @@ public class MRE01V001Z00 extends JFrame implements ActionListener,KeyListener {
 	private GridBagConstraints gbc;
 
 	/** 開始行定義 */
+	protected static int LOW_HAD_INF = 0;
 	protected static int LOW_PAT_INF = 4;
 	private int LOW_CON_INF = 9;
 	private int LOW_INT_INF = 13;
@@ -292,71 +291,16 @@ public class MRE01V001Z00 extends JFrame implements ActionListener,KeyListener {
 		//====　ヘッダー部　===========================================
 		//========================================================
 
-		//１行目
-		JLabel label_1 = new JLabel("新患受付（非がん患者様用）");
-		label_1.setFont(new Font("メイリオ", Font.BOLD, 36));
-		label_1.setPreferredSize(LAYOUT_10BY2);
-		label_1.setHorizontalAlignment(JLabel.CENTER);
-		this.addComp(label_1, 0, 0, 11, 1);
-
-		//２行目(改行のための空白文字)
-		JLabel label_2 = new JLabel("　");
-		label_2.setFont(new Font("メイリオ", Font.PLAIN, 15));
-		this.addComp(label_2, 0, 1, 11, 1);
-
-		//３行目
-		//受付日　ラベル
-		JLabel label_3_1 = new JLabel("<HTML><U>受付日</U></HTML>");
-		label_3_1.setFont(new Font("メイリオ", Font.PLAIN, 22));
-		label_3_1.setForeground(Color.BLUE);
-		label_3_1.setPreferredSize(LAYOUT_1BY1);
-		label_3_1.setHorizontalAlignment(JLabel.RIGHT);
-		this.addComp(label_3_1, 1, 2, 1, 1);
-
-		//受付日　テキスト
-		DatePicker datePicker1 = new DatePicker();
-		datePicker1.setDateToToday();
-		datePicker1.setFont(new Font("メイリオ", Font.PLAIN, 17));
-		datePicker1.setPreferredSize(LAYOUT_1BY1);
-		this.addComp(datePicker1, 2, 2, 1, 1);
-
-		//受付者　ラベル
-		JLabel label_3_4 = new JLabel("<HTML><U>受付者</U></HTML>");
-		label_3_4.setFont(new Font("メイリオ", Font.PLAIN, 22));
-		label_3_4.setForeground(Color.BLUE);
-		label_3_4.setPreferredSize(LAYOUT_1BY1);
-		label_3_4.setHorizontalAlignment(JLabel.RIGHT);
-		this.addComp(label_3_4, 5, 2, 1, 1);
-
-		//受付者　テキスト
-		text_3_5 = new JTextFieldEx(8,0,100,JTextFieldEx.IM_HIRAGANA);
-		text_3_5.setFont(new Font("メイリオ", Font.PLAIN, 17));
-		text_3_5.setPreferredSize(LAYOUT_1BY1);
-		this.addComp(text_3_5, 6, 2, 1, 1);
-
-		//担当　ラベル
-		JLabel label_3_6 = new JLabel("担当");
-		label_3_6.setFont(new Font("メイリオ", Font.PLAIN, 22));
-		label_3_6.setPreferredSize(LAYOUT_1BY1);
-		label_3_6.setHorizontalAlignment(JLabel.RIGHT);
-		this.addComp(label_3_6, 7, 2, 1, 1);
-
-		//担当　テキスト
-		text_3_7 = new JTextFieldEx(8,0,100,JTextFieldEx.IM_HIRAGANA);
-		text_3_7.setFont(new Font("メイリオ", Font.PLAIN, 17));
-		text_3_7.setPreferredSize(LAYOUT_1BY1);
-		this.addComp(text_3_7, 8, 2, 1, 1);
-
-		//４行目(改行のための空白文字)
-		JLabel label_4 = new JLabel("　");
-		label_4.setFont(new Font("メイリオ", Font.PLAIN, 10));
-		this.addComp(label_4, 0, 3, 11, 1);
+		MRE01V002Z00 hadObj = new MRE01V002Z00();
+		hadObj.execute(this,LOW_HAD_INF);
+		text_3_5 = hadObj.getText_3_5();
+		text_3_7 = hadObj.getText_3_7();
 
 		//========================================================
 		//====　患者情報　===========================================
 		//========================================================
 
-		MRE01V002Z00 pstObj = new MRE01V002Z00();
+		MRE01V003Z00 pstObj = new MRE01V003Z00();
 		pstObj.execute(this,LOW_PAT_INF);
 
 		text_5_3 = pstObj.getText_5_3();
@@ -370,205 +314,6 @@ public class MRE01V001Z00 extends JFrame implements ActionListener,KeyListener {
 		combo_8_2 = pstObj.getCombo_8_2();
 		text_8_4 = pstObj.getText_8_4();
 		text_8_6 = pstObj.getText_8_6();
-
-//		//５行目
-//		JLabel label_5_1 = new JLabel("①患者情報");
-//		label_5_1.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_5_1.setPreferredSize(LAYOUT_1BY1);
-//		label_5_1.setHorizontalAlignment(JLabel.LEFT);
-//		this.addComp(label_5_1, 0, LOW_PAT_INF, 1, 1);
-//
-//		//フリガナ ラベル
-//		JLabel label_5_2 = new JLabel("<HTML><U>フリガナ</U></HTML>");
-//		label_5_2.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_5_2.setForeground(Color.BLUE);
-//		label_5_2.setPreferredSize(LAYOUT_1BY1);
-//		label_5_2.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_5_2, 1, LOW_PAT_INF, 1, 1);
-//
-//		//フリガナ　テキスト
-//		text_5_3 = new JTextFieldEx(8,0,40,JTextFieldEx.IM_HALFKANA);
-//		text_5_3.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		text_5_3.setPreferredSize(LAYOUT_1BY1);
-//		this.addComp(text_5_3, 2, LOW_PAT_INF, 1, 1);
-//
-//		//カルテID ラベル
-//		JLabel label_5_4 = new JLabel("カルテID");
-//		label_5_4.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_5_4.setPreferredSize(LAYOUT_1BY1);
-//		label_5_4.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_5_4, 3, LOW_PAT_INF, 1, 1);
-//
-//		//カルテID テキスト
-//		DecimalFormat df = new DecimalFormat("0");
-//		df.setMaximumIntegerDigits(8);
-//		df.setMinimumIntegerDigits(8);
-//		text_5_5 = new JFormattedTextField(df);
-//		text_5_5.setColumns(8);
-//		text_5_5.addActionListener(this);
-//		text_5_5.setActionCommand("pat_inf_pid");
-//		text_5_5.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		text_5_5.setPreferredSize(LAYOUT_1BY1);
-//		this.addComp(text_5_5, 4, LOW_PAT_INF, 1, 1);
-//
-//		//６行目
-//		JLabel label_6_1 = new JLabel("氏名");
-//		label_6_1.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_6_1.setPreferredSize(LAYOUT_1BY1);
-//		label_6_1.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_6_1, 1, LOW_PAT_INF + 1, 1, 1);
-//
-//		//氏名　テキスト
-//		text_6_2 = new JTextFieldEx(8,0,100,JTextFieldEx.IM_HIRAGANA);
-//		text_6_2.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		text_6_2.setPreferredSize(LAYOUT_1BY1);
-//		this.addComp(text_6_2, 2, LOW_PAT_INF + 1, 1, 1);
-//
-//		JLabel label_6_3 = new JLabel("<HTML><U>性別</U></HTML>");
-//		label_6_3.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_6_3.setForeground(Color.BLUE);
-//		label_6_3.setPreferredSize(LAYOUT_1BY1);
-//		label_6_3.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_6_3, 3, LOW_PAT_INF + 1, 1, 1);
-//
-//		combo_6_4 = new JComboBox<>(comb_6_4);
-//		combo_6_4.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		combo_6_4.setPreferredSize(new Dimension(118, 38));
-//		combo_6_4.addActionListener(this);
-//		combo_6_4.setActionCommand("sex");
-//		this.addComp(combo_6_4, 4, LOW_PAT_INF + 1, 1, 1);
-//
-//		JLabel label_6_5 = new JLabel("<HTML><U>生年月日</U></HTML>");
-//		label_6_5.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_6_5.setForeground(Color.BLUE);
-//		label_6_5.setPreferredSize(LAYOUT_1BY1);
-//		label_6_5.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_6_5, 5, LOW_PAT_INF + 1, 1, 1);
-//
-//		//生年月日 テキスト
-//		MaskFormatter mfPtBrth = null;
-//		try{
-//			mfPtBrth = new MaskFormatter("U##/##/##");
-//			mfPtBrth.setPlaceholderCharacter('_');
-//        }catch(ParseException pe){
-//        }
-//		text_6_6 = new JFormattedTextField(mfPtBrth);
-//		text_6_6.setColumns(7);
-//		text_6_6.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		text_6_6.setPreferredSize(LAYOUT_1BY1);
-//		text_6_6.addActionListener(this);
-//		text_6_6.setActionCommand("pat_inf_bth");
-//		text_6_6.addKeyListener(this);
-//		text_6_6.setFocusTraversalKeysEnabled(false);
-//		this.addComp(text_6_6, 6, LOW_PAT_INF + 1, 1, 1);
-//
-//		JLabel label_6_7 = new JLabel("年齢");
-//		label_6_7.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_6_7.setPreferredSize(LAYOUT_1BY1);
-//		label_6_7.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_6_7, 7, LOW_PAT_INF + 1, 1, 1);
-//
-//		text_6_8 = new JTextField("");
-//		text_6_8.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		text_6_8.setPreferredSize(LAYOUT_1BY1);
-//		this.addComp(text_6_8, 8, LOW_PAT_INF + 1, 1, 1);
-//
-//		JLabel label_6_9 = new JLabel("歳");
-//		label_6_9.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_6_9.setPreferredSize(LAYOUT_1BY1);
-//		label_6_9.setHorizontalAlignment(JLabel.LEFT);
-//		this.addComp(label_6_9, 9, LOW_PAT_INF + 1, 1, 1);
-//
-//		//７行目
-//		JLabel label_7_1 = new JLabel("〒");
-//		label_7_1.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_7_1.setPreferredSize(LAYOUT_1BY1);
-//		label_7_1.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_7_1, 1, LOW_PAT_INF + 2, 1, 1);
-//
-//		//郵便番号 テキスト
-//		MaskFormatter mfPost = null;
-//		try{
-//			mfPost = new MaskFormatter(" ###-####");
-//			mfPost.setPlaceholderCharacter('_');
-//        }catch(ParseException pe){
-//        }
-//		text_7_2 = new JFormattedTextField(mfPost);
-//		text_7_2.setColumns(8);
-//		text_7_2.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		text_7_2.setPreferredSize(LAYOUT_1BY1);
-//		this.addComp(text_7_2, 2, LOW_PAT_INF + 2, 1, 1);
-//
-//		//住所ラベル
-//		JLabel label_7_3 = new JLabel("住所");
-//		label_7_3.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_7_3.setPreferredSize(LAYOUT_1BY1);
-//		label_7_3.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_7_3, 3, LOW_PAT_INF + 2, 1, 1);
-//
-//		//住所テキスト
-//		text_7_4 = new JTextFieldEx(37,0,100,JTextFieldEx.IM_HIRAGANA);
-//		text_7_4.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		text_7_4.setPreferredSize(LAYOUT_5BY1);
-//		this.addComp(text_7_4, 4, LOW_PAT_INF + 2, 5, 1);
-//
-//		//８行目
-//		JLabel label_8_1 = new JLabel("駐車場");
-//		label_8_1.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_8_1.setPreferredSize(LAYOUT_1BY1);
-//		label_8_1.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_8_1, 1, LOW_PAT_INF + 3, 1, 1);
-//
-//		//駐車場 リスト
-//		combo_8_2 = new JComboBox<>(comb_8_2);
-//		combo_8_2.setFont(new Font("メイリオ", Font.PLAIN, 17));
-//		combo_8_2.setPreferredSize(new Dimension(118, 38));
-//		combo_8_2.addActionListener(this);
-//		combo_8_2.setActionCommand("parking");
-//		this.addComp(combo_8_2, 2, LOW_PAT_INF + 3, 1, 1);
-//
-//		//電話番号 ラベル
-//		JLabel label_8_3 = new JLabel("電話番号");
-//		label_8_3.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_8_3.setPreferredSize(LAYOUT_1BY1);
-//		label_8_3.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_8_3, 3, LOW_PAT_INF + 3, 1, 1);
-//
-//		//電話番号　テキスト
-//		MaskFormatter mfPtPhone = null;
-//		try{
-//			mfPtPhone = new MaskFormatter("###-###-####");
-//			mfPtPhone.setPlaceholderCharacter('_');
-//        }catch(ParseException pe){
-//        }
-//		text_8_4 = new JFormattedTextField(mfPtPhone);
-//		text_8_4.setFont(new Font("メイリオ", Font.PLAIN, 15));
-//		text_8_4.setPreferredSize(LAYOUT_1BY1);
-//		this.addComp(text_8_4, 4, LOW_PAT_INF + 3, 1, 1);
-//
-//		//携帯番号 ラベル
-//		JLabel label_8_5 = new JLabel("携帯番号");
-//		label_8_5.setFont(new Font("メイリオ", Font.PLAIN, 22));
-//		label_8_5.setPreferredSize(LAYOUT_1BY1);
-//		label_8_5.setHorizontalAlignment(JLabel.RIGHT);
-//		this.addComp(label_8_5, 5, LOW_PAT_INF + 3, 1, 1);
-//
-//		//携帯番号　テキスト
-//		MaskFormatter mfPtMobile = null;
-//		try{
-//			mfPtMobile = new MaskFormatter("###-####-####");
-//			mfPtMobile.setPlaceholderCharacter('_');
-//        }catch(ParseException pe){
-//        }
-//		text_8_6 = new JFormattedTextField(mfPtMobile);
-//		text_8_6.setFont(new Font("メイリオ", Font.PLAIN, 15));
-//		text_8_6.setPreferredSize(LAYOUT_1BY1);
-//		this.addComp(text_8_6, 6, LOW_PAT_INF + 3, 1, 1);
-//
-//		//９行目(改行のための空白文字)
-//		JLabel label_9 = new JLabel("　");
-//		label_9.setFont(new Font("メイリオ", Font.PLAIN, 10));
-//		this.addComp(label_9, 0, LOW_PAT_INF + 4, 11, 1);
 
 		//========================================================
 		//====　相談者情報　=========================================
